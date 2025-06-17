01100e21f83c81406291b568519e7a76207a4133
Arttu Nikkilä <artnikkila@gmail.com>
2025-06-17 10:59:47 +0300

    add fixes to files

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,22 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-                heroImage: image().optional(),
-                color: z.enum(['red', 'blue', 'yellow']).optional(),
-        }),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      excerpt: z.string().optional(), // ← tämä lisättiin
+
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+      color: z.enum(["red", "blue", "yellow"]).optional(),
+    }),
 });
 
 export const collections = { blog };
